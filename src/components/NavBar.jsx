546ddc221fa55607e9a981e0d8d24c8e04625900
Donghyun Kim <crtmt97@gmail.com>
2023-02-03 04:27:02 +0900

    feat:네비바 장바구니 아이템 갯수 표시기능 추가

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 import User from './User';
 import Button from './ui/Button';
 import { useAuthContext } from './context/AuthContext';
+import { getCart } from '../api/firebase';
+import { useQuery } from '@tanstack/react-query';
+
 const NavBar = () => {
   const { user, login, logout } = useAuthContext();
+  const { data: items } = useQuery(['cart'], () => getCart(uid));
+  console.log(items.length);
   return (
     <header className='flex justify-between p-2 border-b border-gray-300'>
       <Link to='/' className='flex text-4xl items-center text-brand'>
@@ -15,7 +21,15 @@ const NavBar = () => {
       </Link>
       <nav className='flex items-center gap-4 font-semibold'>
         <Link to='/products'>Products</Link>
-        {user && <Link to='/carts'>Carts</Link>}
+        {user && (
+          <Link to='/carts' className='text-3xl relative'>
+            <div className='bg-brand rounded-full text-xs absolute w-4 h-4 text-center left-5'>
+              {items.length}
+            </div>
+            <AiOutlineShoppingCart />
+          </Link>
+        )}
+
         {user && user.isAdmin && (
           <Link to='/products/new'>
             <BsFillPencilFill className='text-2xl' />
